e0ada8d5f541e63a9f7ed0f46e47b252ab535c1c
samuel107152 <samuelcauabarbosa114@gmail.com>
2022-12-13 18:20:28 -0300

    Att de novos icones de carrinho

---
+++ Roupa_infantil_2.0/items/add-3,0/index.js
@@ -0,0 +1,10 @@
+const cartButtons = document.querySelectorAll('.cart-button');
+
+cartButtons.forEach(button => {
+	button.addEventListener('click', cartClick);
+});
+
+function cartClick() {
+	let button = this;
+	button.classList.add('clicked');
+}
